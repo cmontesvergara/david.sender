dd5f337b54945af2d4e6ec53e6c4a452d7955ea7
Carlos Montes <krlosbergara@gmail.com>
2025-08-03 16:50:52 -0500

    update guard

--- src/common/logger/guards/jwt-auth.guard.ts
+++ src/common/logger/guards/jwt-auth.guard.ts
@@ -27,38 +27,51 @@ export class JwtAuthGuard implements CanActivate {
 
     const token = auth.split(' ')[1];
     return new Promise((resolve, reject) => {
-      let jwtVerify: any;
-      try {
-        jwtVerify = jwt.verify(token, this.getKey.bind(this), {
+      jwt.verify(
+        token,
+        this.getKey.bind(this),
+        {
           algorithms: ['RS256'],
-          //audience: process.env.SSO_AUDIENCE,// Uncomment if you want to enforce audience
           issuer: process.env.SSO_DOMAIN,
-        });
-      } catch (error: any) {
-        return reject(new UnauthorizedException(error.message));
-      }
+        },
+        (err, decoded: any) => {
+          if (err) {
+            console.error('JWT verification error:', err);
+            return reject(new UnauthorizedException(err.message));
+          }
 
-      const tokenAud = jwtVerify!.aud;
+          const tokenAud = decoded.aud;
+          const currentHost = process.env.CURRENT_HOST as string;
 
-      if (Array.isArray(tokenAud)) {
-        if (!tokenAud.includes(process.env.CURRENT_HOST as string)) {
-          return reject(new Error('Audiencia inválida para esta API'));
-        }
-      } else if (typeof tokenAud === 'string') {
-        const auds = tokenAud.split(',');
-        if (!auds.includes(process.env.CURRENT_HOST as string)) {
-          return reject(new Error('Audiencia inválida para esta API'));
-        }
-      } else {
-        return reject(new Error('Formato de audiencia inválido'));
-      }
-      req.user = tokenAud;
-      resolve(true);
+          if (Array.isArray(tokenAud)) {
+            if (!tokenAud.includes(currentHost)) {
+              return reject(
+                new UnauthorizedException('Audiencia inválida para esta API'),
+              );
+            }
+          } else if (typeof tokenAud === 'string') {
+            const auds = tokenAud.split(',');
+            if (!auds.includes(currentHost)) {
+              return reject(
+                new UnauthorizedException('Audiencia inválida para esta API'),
+              );
+            }
+          } else {
+            return reject(
+              new UnauthorizedException('Formato de audiencia inválido'),
+            );
+          }
+
+          req.user = decoded;
+          resolve(true);
+        },
+      );
     });
   }
   private getKey(header: jwt.JwtHeader, callback: jwt.SigningKeyCallback) {
     this.client.getSigningKey(header.kid, function (err, key) {
       const signingKey = key?.getPublicKey();
+      console.log('Signing key:', err, signingKey);
       callback(err, signingKey);
     });
   }
